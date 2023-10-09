6bf45fc3dd3aaee8f7152147cc762031084090af
Nolatrae <105476991+Nolatrae@users.noreply.github.com>
2023-10-09 21:42:05 +0800

    prefix

---
+++ project/prefix/prefix.py
@@ -0,0 +1,45 @@
+def is_operator(token):
+    return token in "+-*/"
+
+
+def is_operand(token):
+    return token.isdigit() or (token[0] == '-' and token[1:].isdigit())
+
+
+def precedence(operator):
+    if operator in "+-":
+        return 1
+    if operator in "*/":
+        return 2
+    return 0
+
+
+def prefix_to_infix(expression):
+    tokens = expression.split()
+    stack = []
+
+    for token in reversed(tokens):
+        if is_operand(token):
+            stack.append(token)
+        elif is_operator(token):
+            if len(stack) < 2:
+                raise ValueError("Invalid expression")
+            operand1 = stack.pop()
+            operand2 = stack.pop()
+            infix_expression = f"({operand1} {token} {operand2})"
+            stack.append(infix_expression)
+
+    if len(stack) == 1:
+        return stack[0]
+    else:
+        raise ValueError("Invalid expression")
+
+
+# Ввод префиксного выражения
+prefix_expression = input("Введите префиксное выражение: ")
+
+try:
+    infix_expression = prefix_to_infix(prefix_expression)
+    print(f"Инфиксная запись: {infix_expression}")
+except ValueError as e:
+    print(f"Ошибка: {e}")
---
+++ project/tests/test_prefix.py
@@ -0,0 +1,44 @@
+import pytest
+from prefix.prefix import is_operator, is_operand, precedence, prefix_to_infix
+import prefix as pr
+
+
+def test_is_operator():
+    assert pr.is_operator("+") == True
+    assert pr.is_operator("-") == True
+    assert pr.is_operator("*") == True
+    assert pr.is_operator("/") == True
+    assert pr.is_operator("5") == False
+    assert pr.is_operator("a") == False
+    assert pr.is_operator(" ") == False
+
+
+def test_is_operand():
+    assert is_operand("5") == True
+    assert is_operand("-5") == True
+    assert is_operand("0") == True
+    assert is_operand("-0") == True
+    assert is_operand("a") == False
+    assert is_operand("+") == False
+
+
+def test_precedence():
+    assert precedence("+") == 1
+    assert precedence("-") == 1
+    assert precedence("*") == 2
+    assert precedence("/") == 2
+    assert precedence("a") == 0
+    assert precedence("5") == 0
+
+
+def test_prefix_to_infix():
+    assert prefix_to_infix("+ - 13 4 55") == "((13 - 4) + 55)"
+    assert prefix_to_infix("+ 2 * 2 - 2 1") == "(2 + (2 * (2 - 1)))"
+    assert prefix_to_infix("+ + 10 20 30") == "((10 + 20) + 30)"
+    assert prefix_to_infix("- - 1 2") == "Invalid expression"
+    assert prefix_to_infix(
+        "/ + 3 10 * + 2 3 - 3 5") == "((3 + 10) / ((2 + 3) * (3 - 5)))"
+
+
+if __name__ == '__main__':
+    pytest.main()
